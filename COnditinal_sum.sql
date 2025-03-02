10a89c89a0653ecec7a55f2939d41d22957c6832
SmnKmr01 <109888611+SmnKmr01@users.noreply.github.com>
2025-03-02 23:27:45 +0530

    Add files via upload

---
+++ COnditinal_sum.sql
@@ -0,0 +1,27 @@
+--Conditional Sum
+
+SELECT
+
+o.order_month,
+
+SUM(
+
+CASE WHEN p.status = 'Paid' THEN o.order_amount ELSE 0 END
+
+) AS total_amount_paid,
+
+SUM(
+
+CASE WHEN p.status = 'Pending' THEN o.order_amount ELSE 0 END
+
+) AS total_amount_pending,
+
+SUM(
+
+CASE WHEN p.status = 'Default' THEN o.order_amount ELSE 0 END
+
+) AS total_amount_default
+
+FROM orders o
+
+INNER JOIN payment_status p ON o.payment_status_id = p.id;
