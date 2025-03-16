14fd517d19be95b69653acb0313204eb98414ab4
Suman Kumar Pradhan <109888611+SmnKmr01@users.noreply.github.com>
2025-03-16 17:31:51 +0530

    Add files via upload

---
+++ Filter_room_types.sql
@@ -0,0 +1,30 @@
+/* Find the room types that are searched most no of times.
+Output the room type alongside the number of searches for it.
+If the filter for room types has more than one type,
+consider each unique room type as a separate row.
+Sort the result based on the number of searches in descending order */
+
+create table airbnb_searches 
+(
+user_id int,
+date_searched date,
+filter_room_types varchar(200)
+);
+delete from airbnb_searches;
+insert into airbnb_searches values
+(1,'2022-01-01','entire home,private room')
+,(2,'2022-01-02','entire home,shared room')
+,(3,'2022-01-02','private room,shared room')
+,(4,'2022-01-03','private room')
+;
+
+select * from airbnb_searches
+
+
+--strinf_split
+select value from string_split('entire home, private room',',')
+
+select value as room_type, count(1) as no_of_searches from airbnb_searches
+cross apply string_split(filter_room_types,',')
+group by value
+order by no_of_searches desc
