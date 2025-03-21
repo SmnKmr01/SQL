491a802a253c7b2c0f10d845f09027df6e24f5cc
Suman Kumar Pradhan <109888611+SmnKmr01@users.noreply.github.com>
2025-03-22 00:58:21 +0530

    Add files via upload

---
+++ NTILE.sql
@@ -0,0 +1,8 @@
+--problem statement : find top 25 and bottom 25 percent customers by sales
+
+/*the NTILE Analytical function is used to divide a result set into a specified number of groups or buckets based on a specific */
+
+select * from superstore_orders$
+
+
+
