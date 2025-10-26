4638448041b0584a03e51423aa0234ecf0bed824
Suman Kumar Pradhan <109888611+SmnKmr01@users.noreply.github.com>
2025-10-26 20:50:14 +0530

    Add files via upload

---
+++ SQL60.sql
@@ -0,0 +1,7 @@
+--Show all of the days of the month (1-31) and how many admission_dates occurred on that day. 
+--Sort by the day with most admissions to least admissions.
+
+select DAY(admission_date) as day_number, count(*) as number_of_admissions
+from  admissions 
+group by DAY(admission_date)
+order by number_of_admissions desc
