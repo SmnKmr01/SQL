23380c37fbe2b35202278e0315c785b188b6941f
Suman Kumar Pradhan <109888611+SmnKmr01@users.noreply.github.com>
2026-03-22 21:25:44 +0530

    Add files via upload

---
+++ Practice_14.sql
@@ -0,0 +1,50 @@
+CREATE TABLE Signups (
+    user_id INT,
+    time_stamp DATETIME
+);
+
+CREATE TABLE Confirmations (
+    user_id INT,
+    time_stamp DATETIME,
+    action VARCHAR(20)
+);
+drop table 
+INSERT INTO Signups (user_id, time_stamp) VALUES
+(3, '2020-03-21 10:16:13'),
+(7, '2021-01-04 13:57:59'),
+(2, '2020-07-29 23:09:44'),
+(6, '2020-12-09 10:39:37');
+
+INSERT INTO Confirmations (user_id, time_stamp, action) VALUES
+(3, '2021-01-06 03:30:46', 'timeout'),
+(3, '2021-07-14 14:00:00', 'timeout'),
+(7, '2021-06-12 11:57:29', 'confirmed'),
+(7, '2021-06-13 12:58:28', 'confirmed'),
+(7, '2021-06-14 13:59:27', 'confirmed'),
+(2, '2021-01-22 00:00:00', 'confirmed'),
+(2, '2021-02-28 23:59:59', 'timeout');\
+
+
+select * from Signups
+select * from Confirmations
+
+select s.user_id, 
+	round(avg(Case when c.action = 'confirmed' theN 1 else 0)
+	end),2)) as Confrimation_rate
+from Signups s
+join Confirmations c
+s.user_id = c.user_id
+group by s.user_id
+order s.user_id
+
+SELECT
+    s.user_id,
+    ROUND(AVG(CASE WHEN c.action = 'confirmed' THEN 1.00 ELSE 0 END),2) as confirmation_rate
+FROM
+    Signups s
+LEFT JOIN
+    Confirmations c
+    ON
+    s.user_id = c.user_id
+GROUP BY
+    s.user_id
