749dd82ade2cf8487481b736413e955cb926343a
Suman Kumar Pradhan <109888611+SmnKmr01@users.noreply.github.com>
2026-04-27 20:29:54 +0530

    Add files via upload

---
+++ Practice_34.sql
@@ -0,0 +1,23 @@
+
+
+CREATE TABLE orders (
+    id INT
+);
+
+INSERT INTO orders (id)
+VALUES
+(1),
+(2),
+(4),
+(5);
+
+--find missing sequential order IDs
+
+select * from orders
+
+SELECT o1.id + 1 AS missing_id
+FROM orders o1
+LEFT JOIN orders o2
+    ON o1.id + 1 = o2.id
+WHERE o2.id IS NULL
+AND o1.id < (SELECT MAX(id) FROM orders)
