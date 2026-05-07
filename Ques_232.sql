6638d72f5e7abd9a57deeb4a9c2c55e9d6634c0d
Suman Kumar Pradhan <109888611+SmnKmr01@users.noreply.github.com>
2026-05-07 19:31:08 +0530

    Add files via upload

---
+++ Ques_232.sql
@@ -0,0 +1,26 @@
+
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    order_date DATE,
+    delivery_date DATE
+);
+
+-- Insert Sample Data
+INSERT INTO orders (order_id, customer_name, order_date, delivery_date)
+VALUES
+(1, 'Amit',  '2026-05-01', '2026-05-05'),
+(2, 'Priya', '2026-05-02', '2026-05-06'),
+(3, 'Rahul', '2026-05-03', '2026-05-08'),
+(4, 'Sneha', '2026-05-04', NULL),
+(5, 'Karan', '2026-05-05', '2026-05-07')
+
+select * from orders
+
+--Find the average time difference between order and delivery
+
+SELECT 
+    AVG(DATEDIFF(DAY, order_date, delivery_date) * 1.0) AS avg_delivery_time
+FROM orders
+WHERE delivery_date IS NOT NULL
